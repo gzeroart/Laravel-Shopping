68e7b121cb12943e425c3539b967b5607ff9461e
zero art <zeroart@qq.com>
2019-11-19 16:55:06 +0800

    2019年11月19日16点54分

---
+++ app/Http/Controllers/Admin/CommentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class CommentController extends Controller
+{
+    public function comment()
+    {
+        echo "1";
+    }
+}
